38d95ff26356f759c6f334dfe20b2e7bd499dcc0
sipakov <dimasipakov@gmail.com>
2020-02-02 21:58:32 +0300

    add test

--- FUN/Tests/UnitTest1.cs
+++ FUN/Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using FUN;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -23,6 +24,19 @@ namespace Tests
         {
             string t = "i ran there";
             Assert.Equal("I Ran There", fun.LetterCapitalize(t));
+        }
+
+        [Fact]
+        public void Test_sort_hoara()
+        {
+            List<int> a1 = new List<int> { 5,4,3,2,1 };
+            List<int> a2 = new List<int> { 1,7,2,8,0,9,11 };
+            fun.sort_hoara(ref a1);
+            fun.sort_hoara(ref a2);
+
+            bool test1 = "1,2,3,4,5" == string.Join(",", a1);
+            bool test2 = "0,1,2,7,8,9,11" == string.Join(",", a2);
+            Assert.True(test1 == test2);
         }
     }
 }
